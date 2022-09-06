3537ebfb7ff2d57dee8884839702a05770301d78
Ronal Mendoza <ronalmendozat@outlook.com>
2022-09-06 13:18:28 -0500

    Primer commit

---
+++ back-servicio-rest-edificios/src/main/java/com/ronal/edificios/EdificiosApplication.java
@@ -0,0 +1,13 @@
+package com.ronal.edificios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EdificiosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EdificiosApplication.class, args);
+	}
+
+}
---
+++ back-servicio-rest-edificios/src/test/java/com/ronal/edificios/EdificiosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ronal.edificios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EdificiosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
